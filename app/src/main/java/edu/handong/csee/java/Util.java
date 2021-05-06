a22bf1d0094eb18152bbb3976dbb6b2cd939cc70
hajun <hajun@Hakuna-PC>
2021-05-06 11:04:42 +0900

    Add Util class

---
+++ app/src/main/java/edu/handong/csee/java/Util.java
@@ -0,0 +1,17 @@
+package edu.handong.csee.java;
+
+/**
+ * This is the class that capitilizes user input
+ */
+public class Util {
+
+    /**
+     * This is the method that returns capitilized string
+     * @param stringValue user input
+     * @return stringValue
+     */
+    static String getUppercaseString(String stringValue) {
+        return stringValue.toUpperCase();
+    }
+    
+}
